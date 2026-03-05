d59df034a5af95d3dbe2b4c29e5329f5a5b4a0a2
Tijmen Vos <61244099+DemonicDev@users.noreply.github.com>
2026-03-05 23:58:20 +0000

    Added Human Nodes and partly Customies Support
    - Pseudo Mob Class generation on Runtime for custom Mobs!
    -> Todo:
        - MobAI classes
        - Spawn algo

--- src/DemonicCM/DemonicDev/RegisterNodes/CustomiesNode.php
+++ src/DemonicCM/DemonicDev/RegisterNodes/CustomiesNode.php
@@ -3,8 +3,11 @@
 namespace DemonicCM\DemonicDev\RegisterNodes;
 
 use DemonicCM\DemonicDev\AI\mobAI\hostile;
+use customiesdevs\customies\entity\CustomiesEntityFactory;
+use pocketmine\entity\Entity;
 use pocketmine\entity\EntityDataHelper;
 use pocketmine\entity\EntityFactory;
+use pocketmine\entity\Location;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\utils\Config;
 use DemonicCM\DemonicDev\Main;
@@ -14,8 +17,11 @@ class CustomiesNode
 {
     private $main;
     private $config;
-
     private $mobdata = [];
+
+    private array $aiMap = [
+        "hostile" => hostile::class
+    ];
     public function __construct(Main $main){
         $this->main = $main;
         $this->init();
@@ -27,18 +33,39 @@ class CustomiesNode
         var_dump($this->mobdata);
         $this->registerAll();
     }
-
+    private function pseudoClass($name, $data)
+    {
+        $networkTypeId = $data["id"];
+        $ai = $data["ai"];
+        $aiclass = $this->aiMap[$ai] ?? null;
+        if($aiclass === null){
+            throw new \RuntimeException("unknown ai: {ai}");
+        }
+        $classname = "CM_" . $name;
+        if (!class_exists($classname)) {
+            eval(" 
+                class {$classname} extends {$aiclass} {
+                    public static function getNetworkTypeId(): string {
+                        return '{$networkTypeId}';
+                    }
+                }
+            ");
+        }
+        return $classname;
+    }
     private function registerAll(){
         $factory = EntityFactory::getInstance();
-
         foreach ($this->mobdata as $name => $data) {
+            $classname = $this->pseudoClass($name, $data);
+            CustomiesEntityFactory::getInstance()->registerEntity($classname, $data["id"]);
+            /**  this would be HumanNodeWay btw...
             try{
-                $factory->register(hostile::class, function(World $world, CompoundTag $tag) use($name, $data){
+                $factory->register(hostile::class, function(World $world, CompoundTag $tag) use($name, $data): Entity{
                     return new $data["ai"](EntityDataHelper::parseLocation($tag, $world),  $tag);
                 }, [$data["id"], $name]);
             }catch (\Exception $e){
                 $this->main->getLogger()->error($e->getMessage());
-            }
+            }*/
         }
     }
 }
--- src/DemonicCM/DemonicDev/RegisterNodes/HumanNode.php
+++ src/DemonicCM/DemonicDev/RegisterNodes/HumanNode.php
@@ -3,6 +3,8 @@
 namespace DemonicCM\DemonicDev\RegisterNodes;
 
 use DemonicCM\DemonicDev\Main;
+use pocketmine\entity\EntityFactory;
+use pocketmine\utils\Config;
 
 class HumanNode
 {
@@ -10,5 +12,25 @@ class HumanNode
 
     public function __construct(Main $main){
         $this->main = $main;
+    }
+
+    private function init(){
+        $this->main->saveResource("human_mobs.yml",false);
+        $this->config = new Config($this->main->getDataFolder() . "human_mobs.yml", Config::YAML);
+        $this->mobdata = $this->config->getAll(); //[name => data]
+        var_dump($this->mobdata);
+        $this->registerAll();
+    }
+
+    private function registerAll(){
+        $factory = EntityFactory::getInstance();
+        try{
+            $factory->register(hostile::class, function(World $world, CompoundTag $tag) use($name, $data): Entity{
+                return new $data["ai"](EntityDataHelper::parseLocation($tag, $world),  $tag);
+            }, [$data["id"], $name]);
+        }catch (\Exception $e){
+            $this->main->getLogger()->error($e->getMessage());
+        }
+        }
     }
 }
